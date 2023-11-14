8d7ca585b7da87132f6887e6f993d648c731d47e
thellamiechan <133753350+thellamiechan@users.noreply.github.com>
2023-11-14 14:19:53 +1000

    Add files via upload
    starting a place to practice react and building code

---
+++ App.jsx
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+import { NewTodoForm } from './newTodoForm'
+import "./styles.css"
+
+export default function App() {
+  const [todos, setTodos] = useState([])
+
+  function addTodo(title) {
+    setTodos(currentTodos => {
+      return [
+          ...currentTodos, { id: crypto.randomUUID(), title, completed: false }
+      ]
+  })
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return {...todo, completed}
+        }
+        return todo
+      })
+    })
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      currentTodos.filter(todo => todo.id !== id)
+    })
+  }
+
+  return (
+    <>
+     <NewTodoForm onSubmit={addTodo}/> 
+      <h1 className='header'>Todo List</h1>
+      <ul className='list'>
+        {todos.length === 0 && 'No Tasks'}
+        {todos.map(todo => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input
+                  type='checkbox'
+                  checked={todo.completed}
+                  onChange={e => toggleTodo(todo.id, e.target.checked)}
+                />
+                {todo.title}
+              </label>
+              <button 
+              onClick={() => deleteTodo(todo.id)} 
+              className='btn btn-danger'
+              > 
+              Delete 
+              </button>
+            </li>
+          )
+        })}
+      </ul>
+    </>
+  )
+}
